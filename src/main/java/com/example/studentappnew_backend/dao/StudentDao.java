a9f3c345b38a903a86044dfbeb5f18c0dce1ae78
AmalAlby <amalalbyn@gmail.com>
2022-12-08 15:03:26 +0530

    search operation added

--- src/main/java/com/example/studentappnew_backend/dao/StudentDao.java
+++ src/main/java/com/example/studentappnew_backend/dao/StudentDao.java
@@ -1,7 +1,16 @@
 package com.example.studentappnew_backend.dao;
 
 import com.example.studentappnew_backend.model.Students;
+import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.CrudRepository;
+import org.springframework.data.repository.query.Param;
+
+import java.util.List;
 
 public interface StudentDao extends CrudRepository<Students,Integer> {
+
+    @Query(value = "SELECT `id`, `admno`, `college`, `name`, `rollno` FROM `students` WHERE `admno` =:admno",nativeQuery = true)
+    List<Students> SearchStudents(@Param("admno") String admno);
+
+
 }
